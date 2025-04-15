66814ed393c7cf8bf2401a3449fbb038f602c4c4
artem <artem006661artem@gmail.com>
2025-04-15 11:50:50 +0200

    Created Unit Tests for Mapper LanguageTranslationDtMapper

--- service/src/test/java/greencity/mapping/LanguageTranslationDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/LanguageTranslationDtoMapperTest.java
@@ -1,15 +1,37 @@
 package greencity.mapping;
 
+import greencity.ModelUtils;
+import greencity.dto.language.LanguageDTO;
+import greencity.dto.language.LanguageTranslationDTO;
+import greencity.entity.HabitFactTranslation;
+import greencity.enums.FactOfDayStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(SpringExtension.class)
-public class LanguageTranslationDtoMapper {
+public class LanguageTranslationDtoMapperTest {
     @InjectMocks
     private LanguageTranslationDtoMapper languageTranslationDtoMapper;
 
     @Test
-    
+    void convertTest() {
+        HabitFactTranslation habitFactTranslation = ModelUtils.getFactTranslation();
+
+        LanguageTranslationDTO expected = LanguageTranslationDTO.builder()
+                .content(habitFactTranslation.getContent())
+                .language(LanguageDTO.builder()
+                        .id(habitFactTranslation.getLanguage().getId())
+                        .code(habitFactTranslation.getLanguage().getCode())
+                        .build())
+                .build();
+        ;
+
+        assertEquals(expected, languageTranslationDtoMapper.convert(habitFactTranslation));
+
+
+    }
 }
