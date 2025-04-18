0019d07728a8bd6bb1fc07596c0c9d542aab2928
Dmytro_Kravchuk <dmitriy.n.kravchuk@gmail.com>
2025-04-18 21:08:07 +0300

    added 1 plaece after tag <p>

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -589,7 +589,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     /**
      * Exception handler for {@link AccessDeniedException}.
      *
-     * <p>Returns HTTP 403 (FORBIDDEN) when a user tries to perform an action without sufficient permissions.
+     * <p> Returns HTTP 403 (FORBIDDEN) when a user tries to perform an action without sufficient permissions.
      *
      * @param ex the thrown {@link AccessDeniedException}.
      * @return a {@link ResponseEntity} with HTTP status 403 and error message in the body.
