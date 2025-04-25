c0f08e7a626475f0ace3c66906ace14356886a4a
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 11:42:20 +0300

    [87] add `NewsSubscriberService` interface for news subscription

---
+++ service-api/src/main/java/greencity/service/NewsSubscriberService.java
@@ -0,0 +1,15 @@
+package greencity.service;
+
+import greencity.dto.newssubscriber.NewsSubscriberRequestDto;
+
+public interface NewsSubscriberService {
+
+    /**
+     * Method for subscribing to the news.
+     *
+     * @param newsSubscriberRequestDto - email of the subscriber
+     * @return {@link NewsSubscriberRequestDto} - email of the subscriber
+     */
+    NewsSubscriberRequestDto subscribe(NewsSubscriberRequestDto newsSubscriberRequestDto);
+
+}
