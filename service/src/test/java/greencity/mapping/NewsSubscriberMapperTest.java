f5b300a0b0f23ed5b24171b2d4bd608607a1066c
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 11:43:10 +0300

    [87] add `NewsSubscriberMapper` for converting `NewsSubscriberRequestDto` to `NewsSubscriber` and corresponding tests

---
+++ service/src/test/java/greencity/mapping/NewsSubscriberMapperTest.java
@@ -0,0 +1,32 @@
+package greencity.mapping;
+
+import greencity.dto.newssubscriber.NewsSubscriberRequestDto;
+import greencity.entity.NewsSubscriber;
+import org.junit.jupiter.api.Test;
+
+import static greencity.ModelUtils.TEST_EMAIL;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsSubscriberMapperTest {
+
+    private final NewsSubscriberMapper newsSubscriberMapper = new NewsSubscriberMapper();
+
+    @Test
+    void convert_ValidEmail_NewsSubscriber() {
+        NewsSubscriberRequestDto newsSubscriberRequestDto = new NewsSubscriberRequestDto(TEST_EMAIL);
+
+        NewsSubscriber expected = NewsSubscriber.builder()
+                .email(TEST_EMAIL)
+                .build();
+        var actual = newsSubscriberMapper.convert(newsSubscriberRequestDto);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void convert_Null_NullPointerExceptionThrown() {
+        NewsSubscriberRequestDto newsSubscriberRequestDto = null;
+
+        assertThrows(NullPointerException.class, () -> newsSubscriberMapper.convert(newsSubscriberRequestDto));
+    }
+}
