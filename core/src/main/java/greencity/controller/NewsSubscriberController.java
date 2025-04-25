269455ac3416627de96c3c6106a612cdd4c02b36
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 11:44:23 +0300

    [87] add `NewsSubscriberController` for handling subscription requests and corresponding tests

---
+++ core/src/main/java/greencity/controller/NewsSubscriberController.java
@@ -0,0 +1,65 @@
+package greencity.controller;
+
+import greencity.constant.HttpStatuses;
+import greencity.dto.newssubscriber.NewsSubscriberRequestDto;
+import greencity.service.NewsSubscriberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/newsSubscriber")
+@RequiredArgsConstructor
+public class NewsSubscriberController {
+
+    private final NewsSubscriberService newsSubscriberService;
+
+    @PostMapping
+    @Operation(
+            summary = "Subscribe to the news",
+            description = "This method allows you to subscribe to the news by providing an email address."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = HttpStatuses.OK,
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = NewsSubscriberRequestDto.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = HttpStatuses.BAD_REQUEST,
+                            content = {
+                                    @Content(schema = @Schema(hidden = true))
+                            }
+                    ),
+            }
+    )
+    public ResponseEntity<NewsSubscriberRequestDto> post(
+            @Valid
+            @RequestBody
+            @Parameter(
+                    description = "Email of the subscriber",
+                    required = true
+            )
+            NewsSubscriberRequestDto request
+    ) {
+        return ResponseEntity.ok(newsSubscriberService.subscribe(request));
+    }
+
+}
---
+++ core/src/test/java/greencity/controller/NewsSubscriberControllerTest.java
@@ -0,0 +1,87 @@
+package greencity.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import greencity.dto.newssubscriber.NewsSubscriberRequestDto;
+import greencity.service.NewsSubscriberService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import static greencity.ModelUtils.getObjectMapper;
+import static greencity.TestConst.EMAIL;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class NewsSubscriberControllerTest {
+
+    @Mock
+    private NewsSubscriberService newsSubscriberService;
+
+    @InjectMocks
+    private NewsSubscriberController newsSubscriberController;
+
+    @Mock
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = getObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.afterPropertiesSet();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(newsSubscriberController)
+                .setValidator(localValidatorFactoryBean)
+                .build();
+    }
+
+    @Test
+    @SneakyThrows
+    void post_ValidInputData_Ok() {
+        String json = """
+                {
+                  "email": "%s"
+                }
+                """.formatted(EMAIL);
+
+        NewsSubscriberRequestDto response = objectMapper.readValue(json, NewsSubscriberRequestDto.class);
+        when(newsSubscriberService.subscribe(any(NewsSubscriberRequestDto.class))).thenReturn(response);
+
+        mockMvc.perform(post("/newsSubscriber")
+                        .contentType("application/json")
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(EMAIL));
+
+        verify(newsSubscriberService, times(1)).subscribe(any(NewsSubscriberRequestDto.class));
+    }
+
+    @Test
+    @SneakyThrows
+    void post_InvalidEmail_BadRequest() {
+        String json = """
+                {
+                  "email": "invalid-email"
+                }
+                """;
+
+        mockMvc.perform(post("/newsSubscriber")
+                        .contentType("application/json")
+                        .content(json))
+                .andExpect(status().isBadRequest());
+
+        verify(newsSubscriberService, never()).subscribe(any(NewsSubscriberRequestDto.class));
+    }
+
+}
