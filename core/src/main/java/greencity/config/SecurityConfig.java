c0829a654eabadb693c6e101d053a1efddb9baaa
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 11:45:56 +0300

    [87] add `/newsSubscriber` endpoint to security configuration for public access

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -155,7 +155,8 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST,
                     "/ownSecurity/signUp",
                     "/ownSecurity/signIn",
-                    "/ownSecurity/changePassword")
+                    "/ownSecurity/changePassword",
+                    "/newsSubscriber")
                 .permitAll()
                 .requestMatchers(HttpMethod.GET,
                     "/achievements",
@@ -212,7 +213,6 @@ public class SecurityConfig {
                     "/habit/assign/{habitAssignId}/enroll/**",
                     "/habit/assign/{habitAssignId}/unenroll/{date}",
                     "/habit/statistic/{habitId}",
-                    "/newsSubscriber",
                     USER_CUSTOM_SHOPPING_LIST_ITEMS,
                     USER_SHOPPING_LIST,
                     "/user/{userId}/habit",
@@ -254,7 +254,6 @@ public class SecurityConfig {
                     USER_SHOPPING_LIST + "/user-shopping-list-items")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.GET,
-                    "/newsSubscriber",
                     "/comments",
                     "/comments/{id}",
                     "/user/all",
