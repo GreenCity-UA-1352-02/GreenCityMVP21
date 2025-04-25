317c7159141cd87e9a56cee4c0ba5f96023f2a63
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 17:22:52 +0300

    [92] add save method to `EventService` for event creation with images

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -1,4 +1,19 @@
 package greencity.service;
 
+import greencity.dto.event.AddEventDtoRequest;
+import greencity.dto.event.EventDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface EventService {
+    /**
+     * Method saves event.
+     *
+     * @param addEventDtoRequest - event to be saved
+     * @param images             - images of event
+     * @param email              - email of user
+     * @return saved event
+     */
+    EventDto save(AddEventDtoRequest addEventDtoRequest, List<MultipartFile> images, String email);
 }
