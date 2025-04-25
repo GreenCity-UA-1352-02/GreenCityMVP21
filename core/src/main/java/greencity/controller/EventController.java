d62135f1f67ccc4758b8c996928dfe57cb4709b5
makandrii <makarov.andrii.10@gmail.com>
2025-04-25 17:22:24 +0300

    [92] add event creation with multipart support

--- core/src/main/java/greencity/controller/EventController.java
+++ core/src/main/java/greencity/controller/EventController.java
@@ -1,13 +1,35 @@
 package greencity.controller;
 
-import lombok.AllArgsConstructor;
+import greencity.dto.event.AddEventDtoRequest;
+import greencity.dto.event.EventDto;
+import greencity.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
+import java.util.List;
 
 @Validated
 @RestController
-@RequestMapping("/event")
-@AllArgsConstructor
+@RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
+    private final EventService eventService;
+
+    @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<EventDto> post(
+            @RequestPart AddEventDtoRequest addEventDtoRequest,
+            @RequestPart(required = false) List<MultipartFile> images,
+            Principal principal
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(eventService.save(addEventDtoRequest, images, principal.getName()));
+    }
 }
