7e0db7c749deb5e30312f0e667b08e1ff387a6a1
BohdanLys <mrboba04@gmail.com>
2025-04-29 13:08:36 +0300

    Update retreiving notifications order by timestamp

--- dao/src/main/java/greencity/repository/NotificationRepo.java
+++ dao/src/main/java/greencity/repository/NotificationRepo.java
@@ -3,9 +3,12 @@ package greencity.repository;
 import greencity.entity.Notification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepo extends JpaRepository<Notification, Long> {
-    List<Notification> findNotificationByUserId(Long id);
+    @Query("SELECT n FROM Notification n WHERE n.userId = :userId ORDER BY n.timestamp ASC")
+    List<Notification> findNotificationByUserId(@Param("userId") Long id);
 }
