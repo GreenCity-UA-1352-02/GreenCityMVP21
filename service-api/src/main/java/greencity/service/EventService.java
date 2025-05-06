eef6d427f85590842bc280a4545d55c2cc24f2f2
makandrii <makarov.andrii.10@gmail.com>
2025-05-06 16:08:01 +0300

    [92] refactor EventService: rename parameter from AddEventDtoRequest to AddEventRequest for consistency

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -1,6 +1,6 @@
 package greencity.service;
 
-import greencity.dto.event.AddEventDtoRequest;
+import greencity.dto.event.AddEventRequest;
 import greencity.dto.event.EventDto;
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,12 +9,12 @@ public interface EventService {
     /**
      * Method saves event.
      *
-     * @param addEventDtoRequest - event to be saved
+     * @param addEventRequest - event to be saved
      * @param images             - images of event
      * @param email              - email of user
      * @return saved event
      */
-    EventDto save(AddEventDtoRequest addEventDtoRequest, List<MultipartFile> images, String email);
+    EventDto save(AddEventRequest addEventRequest, List<MultipartFile> images, String email);
 
 
     /**
