2d197181cdf36eec13c01cb8229ac9efc477679a
Olexandr <Mega.alex.alex2000@gmail.com>
2025-05-17 23:11:15 +0300

    [129] add EventVO class

---
+++ service-api/src/main/java/greencity/dto/event/EventVO.java
@@ -0,0 +1,30 @@
+package greencity.dto.event;
+
+import greencity.dto.tag.TagVO;
+import greencity.dto.user.UserVO;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(exclude = {"dateLocations", "titleImage", "images", "tags"})
+@ToString(exclude = {"dateLocations", "titleImage", "images", "tags"})
+public class EventVO {
+    private Long id;
+    private String title;
+    private String description;
+    @Builder.Default
+    private boolean isOpen = true;
+    @Builder.Default
+    private List<EventDateLocationDto> dateLocations = new ArrayList<>();
+    private EventImageDto titleImage;
+    @Builder.Default
+    private List<EventImageDto> images = new ArrayList<>();
+    private UserVO user;
+    @Builder.Default
+    private List<TagVO> tags = new ArrayList<>();
+}
