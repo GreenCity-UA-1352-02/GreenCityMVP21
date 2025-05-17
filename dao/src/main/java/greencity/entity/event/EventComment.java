a2d24507142960456f9f4c1a78c77da5e2ef5f9a
Olexandr <Mega.alex.alex2000@gmail.com>
2025-05-17 21:21:27 +0300

    [129] add EventComment entity

---
+++ dao/src/main/java/greencity/entity/event/EventComment.java
@@ -0,0 +1,54 @@
+package greencity.entity.event;
+
+import greencity.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "event_comment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class EventComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(min = 1, max = 8000)
+    private String text;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "modified_date", nullable = false)
+    private LocalDateTime modifiedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_comment_id")
+    private EventComment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = {CascadeType.ALL})
+    private List<EventComment> comments = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @Column
+    private boolean deleted;
+}
