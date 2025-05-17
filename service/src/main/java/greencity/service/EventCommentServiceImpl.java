f19c7ae72c12c86a1bce094b423a3bda7f8f34e8
Olexandr <Mega.alex.alex2000@gmail.com>
2025-05-17 23:14:18 +0300

    [129]  implemented comment saving

--- service/src/main/java/greencity/service/EventCommentServiceImpl.java
+++ service/src/main/java/greencity/service/EventCommentServiceImpl.java
@@ -1,11 +1,20 @@
 package greencity.service;
 
+import static greencity.constant.AppConstant.AUTHORIZATION;
+import greencity.annotations.RatingCalculationEnum;
+import greencity.constant.ErrorMessage;
+import greencity.dto.event.EventVO;
 import greencity.dto.eventcomment.AddEventCommentDtoRequest;
 import greencity.dto.eventcomment.AddEventCommentDtoResponse;
 import greencity.dto.user.UserVO;
+import greencity.entity.User;
+import greencity.entity.event.Event;
+import greencity.entity.event.EventComment;
+import greencity.exception.exceptions.BadRequestException;
 import greencity.repository.EventCommentRepository;
 import greencity.repository.EventRepo;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -15,13 +24,30 @@ import org.springframework.stereotype.Service;
 public class EventCommentServiceImpl implements EventCommentService {
     private EventCommentRepository eventCommentRepo;
     private EventService eventService;
-    private EventRepo eventRepo;
     private ModelMapper modelMapper;
     private final greencity.rating.RatingCalculation ratingCalculation;
     private final HttpServletRequest httpServletRequest;
 
     @Override
     public AddEventCommentDtoResponse save(Long eventId, AddEventCommentDtoRequest comment, UserVO user) {
-        return null;
+        EventVO eventVO = eventService.findById(eventId);
+        EventComment eventComment = modelMapper.map(comment, EventComment.class);
+        eventComment.setUser(modelMapper.map(user, User.class));
+        eventComment.setEvent(modelMapper.map(eventVO, Event.class));
+        if (comment.parentCommentId() != 0) {
+            EventComment parentComment =
+                eventCommentRepo.findById(comment.parentCommentId()).orElseThrow(
+                    () -> new BadRequestException(ErrorMessage.COMMENT_NOT_FOUND_EXCEPTION));
+            if (parentComment.getParentComment() == null) {
+                eventComment.setParentComment(parentComment);
+            } else {
+                throw new BadRequestException(ErrorMessage.CANNOT_REPLY_THE_REPLY);
+            }
+        }
+        String accessToken = httpServletRequest.getHeader(AUTHORIZATION);
+        CompletableFuture.runAsync(
+            () -> ratingCalculation
+                .ratingCalculation(RatingCalculationEnum.ADD_COMMENT, user, accessToken));
+        return modelMapper.map(eventCommentRepo.save(eventComment), AddEventCommentDtoResponse.class);
     }
 }
