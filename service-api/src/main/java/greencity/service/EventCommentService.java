e8c381e6fb722a7ec656a06579109982709ddad5
Olexandr <Mega.alex.alex2000@gmail.com>
2025-05-19 14:00:25 +0300

    [129] add documentation in EventCommentService

--- service-api/src/main/java/greencity/service/EventCommentService.java
+++ service-api/src/main/java/greencity/service/EventCommentService.java
@@ -13,11 +13,23 @@ public interface EventCommentService {
      * @param comment the comment that is added
      * @param user    the owner of the comment
      * @return {@link AddEventCommentDtoResponse} instance
-     * @author Pohranychnyi Olexandr
      */
     AddEventCommentDtoResponse save(Long eventId, AddEventCommentDtoRequest comment, UserVO user);
 
+    /**
+     * Method to change the existing comment.
+     *
+     * @param text new text of the comment
+     * @param id id of the comment that user wants to change
+     * @param user current user {@link  UserVO} that want to change comment
+     */
     void update(@NotBlank String text, Long id, UserVO user);
 
+    /**
+     * Method to mark comment ad deleted.
+     *
+     * @param id id of the comment
+     * @param user current user {@link  UserVO} that want to delete comment
+     */
     void deleteById(Long id, UserVO user);
 }
